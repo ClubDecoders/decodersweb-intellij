11c202424e375624a4dea401db81641fa182f1db
Anurag Ambuj <anuragambuj000@gmail.com>
2016-06-06 14:02:06 +0530

    Updated README

--- src/club/decoders/utils/PasswordUtility.java
+++ src/club/decoders/utils/PasswordUtility.java
@@ -1,14 +1,25 @@
 package club.decoders.utils;
 
+import java.util.Random;
+
 public class PasswordUtility {
-	
-	public static boolean matchPassword(String pass,String conf)
-	{
-		if(pass.equals(conf))
-			return true;
-		else
-			return false;
-	}
-	
-	
+
+    private static String usn;
+
+    public static boolean matchPassword(String pass, String conf) {
+        if (pass.equals(conf))
+            return true;
+        else
+            return false;
+    }
+
+    public static String generateRandomPassword(String cusn) {
+
+        //TODO Replace with an algorithm that generates secure passwords
+        byte[] busn = cusn.getBytes();
+        new Random().nextBytes(busn);
+        return new String(busn);
+    }
 }
+
+
